7fb8e764c96e5718303de262062dffb9cddd2c36
kiryl.zyusko <kiryl.ziusko@itechart-group.com>
2019-12-05 11:15:50 +0300

    [RNBS-018] - deploy instructions updated

--- dp.sh
+++ dp.sh
@@ -1,7 +1,6 @@
 cd website
 npm run build
-<meta name="google-site-verification" content="ntb09WlwTjLqEM0zi_UtNNzyzj8nlxeiPbqxlocq1QI" />
 cd ../
 git checkout -b gh-pages
-git add -f website/build/react-native-bundle-splitter-docs && git commit -m "Initial dist subtree commit"
-git subtree push --prefix website/build/react-native-bundle-splitter-docs origin gh-pages
+git add -f website/build/react-native-bundle-splitter && git commit -m "Initial dist subtree commit"
+git subtree push --prefix website/build/react-native-bundle-splitter origin gh-pages
