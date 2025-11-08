f330c7dcb452ba1232da144eb5f35eb4be09f8a1
kirillzyusko <zyusko.kirik@gmail.com>
2025-11-08 18:39:04 +0100

    fix: TS compilation issues

--- src/utils.ts
+++ src/utils.ts
@@ -1,27 +1,27 @@
-import { Platform } from 'react-native';
+import { Platform } from "react-native";
 // quick and dirty
 declare var require: any;
 
 export const investigate = () => {
-    if (Platform.OS === 'web' || !__DEV__) {
-        // prevent crash in release and web
-        // this function will not work on web and in release
-        return { loaded: [], waiting: [] }
-    }
+  if (Platform.OS === "web" || !__DEV__) {
+    // prevent crash in release and web
+    // this function will not work on web and in release
+    return { loaded: [], waiting: [] };
+  }
 
-    const modules = require.getModules();
-    const loaded = [];
-    const waiting = [];
-    if (modules instanceof Map) {
-        for (const [key, module] of modules) {
-            (module.isInitialized ? loaded : waiting).push(module.verboseName);
-        }
-    } else {
-        for (const key of Object.keys(modules)) {
-            const module = modules[key];
-            (module.isInitialized ? loaded : waiting).push(module.verboseName);
-        }
+  const modules = require.getModules();
+  const loaded: string[] = [];
+  const waiting: string[] = [];
+  if (modules instanceof Map) {
+    for (const [key, module] of modules) {
+      (module.isInitialized ? loaded : waiting).push(module.verboseName);
     }
+  } else {
+    for (const key of Object.keys(modules)) {
+      const module = modules[key];
+      (module.isInitialized ? loaded : waiting).push(module.verboseName);
+    }
+  }
 
-    return { loaded, waiting }
+  return { loaded, waiting };
 };
